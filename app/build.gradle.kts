1b1ee94350e7d46b6055e1ce0d6480aa2cf25501
muchenjiang <183024981+muchenjiang@users.noreply.github.com>
2026-03-31 23:32:00 -0300

    version code 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -198,7 +198,7 @@ android {
         applicationId = "com.lavacrafter.maptimelinetool"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
+        versionCode = 14
         versionName = "0.1.6"
     }
 
